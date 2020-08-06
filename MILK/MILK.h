2bed04d88fd6a83494019c832fc9e9f8ba08db13
Haopeng <philthinker@sina.com>
2020-08-06 10:19:54 +0800

    A brave man

--- MILK/MILK.h
+++ MILK/MILK.h
@@ -2,4 +2,3 @@
 
 double cosInterp(double x0, double xg, double t);
 std::array<double,16> vectorP2arrayCarte(std::vector<double> vectorIn,std::array<double,16> carteIn);
-std::array<double,16> Matrix3d2array16(Eigen::Matrix3d matrixIn);
